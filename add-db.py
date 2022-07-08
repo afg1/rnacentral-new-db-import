8063335ce8bc965bbff5712ea708e1e587e6bef1
Andrew Green <agreen@ebi.ac.uk>
2022-07-08 11:36:57 +0100

    Automate changes to the xref trigger function

--- add-db.py
+++ add-db.py
@@ -4,22 +4,69 @@ import click
 import chevron
 from datetime import datetime, timezone
 import psycopg2 as pg
+import re
 
 
-def run_command(db, command_string):
-    conn = pg.connect(db)
-    cur = conn.cursor()
-    cur.execute(command_string)
+class DBContext:
+    def __init__(self, db_url):
+        self.connection = pg.connect(db_url)
 
-    cur.commit()
+    def run_command(self, command_string):
+        cur = self.connection.cursor()
+        try:
+            cur.execute(command_string)
+        except pg.ProgrammingError as e:
+            print("Syntax probably wrong:")
+            print(e.pgerror)
+            print(command_string)
+            print("Aborting...")
+            exit()
+        except pg.Error as e:
+            print("Some other error")
+            print(e.pgerror)
+            print(command_string)
+            exit()
+        except pg.Warning as w:
+            print(w.pgerror)
 
-def get_next_index(db):
-    conn = pg.connect(db)
-    cur = conn.cursor()
-    cur.execute("select max(id) from rnc_database")
+        return cur
+
+    def commit_changes(self):
+        self.connection.commit()
+
+    def rollback_changes(self):
+        self.connection.rollback()
+
+
+
+def get_next_index(context):
+    cur = context.run_command("select max(id) from rnc_database")
     return cur.fetchone()[0] + 1
 
+def get_xref_trigger_fcn(context):
+    cur = context.run_command("select pg_get_functiondef('rnacen.xref_insert_trigger()'::regprocedure)")
+    return cur.fetchone()[0]
 
+
+def splice_xref_function(original, insertion, index):
+
+    # first find the elif matching the last index
+    regex_elsif = re.compile("^\s*(?:ELS)*IF \( NEW\.DB", re.MULTILINE)
+    last_elif_start = None
+    for idx, p in enumerate(regex_elsif.finditer(original), 1):
+        if idx == index - 1:
+            last_elif_start = p.start()
+
+    ## find the end of the last index elif block
+    regex_endif = re.compile("END IF;\n")
+
+    end_match = regex_endif.search(original, last_elif_start)
+    insert_start = end_match.end()
+
+    # insert our modification in the right place
+    modified_fcn = "".join((original[:insert_start], insertion, original[insert_start:]))
+
+    return modified_fcn
 
 @click.command()
 @click.option("--index", default=None, type=int, help="Enter the database index")
@@ -34,8 +81,14 @@ def main(index, descr, full_descr, display_name, db, dry_run, print_rendered):
     Quick utility to produce and run necessary SQL commands
     for adding a new database in RNAcentral
     """
+
+    context = DBContext(db)
+
     if index is None:
-        index = get_next_index(db)
+        index = get_next_index(context)
+
+    trigger_fcn = get_xref_trigger_fcn(context)
+
     if len(descr) >= 60:
         print("Description string too long! (< 60)")
         return 1
@@ -66,6 +119,7 @@ def main(index, descr, full_descr, display_name, db, dry_run, print_rendered):
     with open("templates/xref-function.mustache", 'r') as xref_template:
         xref = chevron.render(xref_template, argument_dict)
 
+    xref_new = splice_xref_function(trigger_fcn, xref, index)
 
     if dry_run or print_rendered:
         print("Rendered INSERT command as:")
@@ -75,7 +129,7 @@ def main(index, descr, full_descr, display_name, db, dry_run, print_rendered):
         print(partition)
         print("----------------------------------")
         print("Rendered xref function as:")
-        print(xref)
+        print(xref_new)
         print("----------------------------------")
 
     if not dry_run:
@@ -89,9 +143,9 @@ def main(index, descr, full_descr, display_name, db, dry_run, print_rendered):
             print("Aborting...")
         else:
             print("Running commands...")
-            run_command(db, entry)
-            run_command(db, partition)
-            run_command(db, xref)
+            context.run_command(entry)
+            context.run_command(partition)
+            context.run_command(xref_new)
             print("Done!")
 
 
